58dfe355ab961c2562e2d52c80638e29e3a6eac2
Andrew Zimmer <andrew@broadinstitute.org>
2023-03-28 13:00:18 -0400

    WIP

--- pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/constants/ConfigFile.java
+++ pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/constants/ConfigFile.java
@@ -216,6 +216,6 @@ public class ConfigFile {
     }
 
     public static class Test {
-        // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     }
 }
